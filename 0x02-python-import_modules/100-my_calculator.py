d6444b6f3db2da6e25fd760b83f1738071bbcf3d
victorpropython <victorchibuzor3109@gmail.com>
2023-12-27 18:51:19 +0100

    for task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,1 +1,19 @@
+#!/usr/bin/python3
 
+if __name__ == "__main__":
+    """to import all function from calculator_1"""
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    oprs = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(oprs.keys()):
+        print("Unknown operator. Availiable operators: +, -, * and /")
+        sys.exit(1)
+
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        print(f"{a} {sys.argv[2]} {b} = {sys.argv[2](a, b)}")
