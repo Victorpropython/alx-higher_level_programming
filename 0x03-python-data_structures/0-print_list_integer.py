baacc403c5f7f835903eaa82701fc5dc3267940a
victorpropython <victorchibuzor3109@gmail.com>
2024-01-05 03:19:33 +0100

    for task 0 & 1

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    """ T print all intergers  of a list"""
+    for num in range(len(my_list)):
+        print("{:d}".format(num))
---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    """To retrieve an element @ feom a list in c"""
+    if idx < 0:
+        return None
+
+    if idx > (len(my_list) - 1):
+        return None
+    else:
+        return(my_list[idx])
+
