9e72c4a44b96e0be49f1d51712ebbf0aba720cf7
victorpropython <victorchibuzor3109@gmail.com>
2024-02-12 01:07:05 +0100

    for task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 
 def square_matrix_simple(matrix=[]):
     """ To compute the squares values of all integer in matrix"""
-    new_matrix = [[x **2 for x in row] for row in matrix]
+    new_matrix = [[x ** 2 for x in row] for row in matrix]
     return new_matrix
