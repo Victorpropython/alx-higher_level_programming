89cb1ae74e66bb216266c6b9881941a550593a3d
victorpropython <victorchibuzor3109@gmail.com>
2024-02-12 17:11:51 +0100

    my second commit for task 1 to 4

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,10 @@
 #!/bin/bash/python3
+"""Function that return a set
+of common element in two set
+"""
 
-"""Function that return a set of common element in two set"""
+
 def common_elements(set_1, set_2):
     a = set_1
     b = set_2
-    return (a & b)
+    return a & b
