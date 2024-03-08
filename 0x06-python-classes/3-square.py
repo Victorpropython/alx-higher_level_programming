c79063441b5f5b6d91ab526f66b83b78013c00bc
victorpropython <victorchibuzor3109@gmail.com>
2024-03-08 21:10:51 +0100

    for task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" To return current Square area"""
+
+
+class Square:
+    """
+    Function to calculate the area of a square
+    using a private class attribute
+    :size: is the private class attribute
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return (self.__size * 2)
