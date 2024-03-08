80e0622a98236c9092b894d202d0d50d5bd2c19e
victorpropython <victorchibuzor3109@gmail.com>
2024-03-08 20:52:50 +0100

    for task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,12 +5,11 @@
 class Square:
     """
     Function to access th private attribute class square
-    :size: the attribute to be accessed privately and it can be 
+    :size: the attribute to be accessed privately and it can be
     updated, retrieve and validates.
     """
 
-
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
