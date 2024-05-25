1eb95d16fb29d112695e157709c6dc252b27dcbe
victorpropython <victorchibuzor3109@gmail.com>
2024-05-25 17:40:40 +0100

    for test 2

---
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+#test_rectangle.py
+#Victor chibuike
+"""Defines unittest for models/rectangle.py.
+
+Unittest classes:
+    TestRectangle_instantiation - line 
+    TestRectangle_width - line
+    TestRectangle_height - line
+    TestRectangle_x - line
+    TestRectangle_y - line
+    TestRectangle_order_of_instantiation - line
+    TestRectangle_area - line
+    TestRectangle_update_args - line
+    TestRectangle_update_kwargs - line
+    TestRectangle_to_dictionary - line
+"""
+import io
+import sys
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+class TestRectangle_instantiation(unittest.TestCase):
+    """Unitest to test instantiation of the Rectangle class."""
+
+    def test_rectangle_is_base(self):
+        self.assertIsInstances(Rectangle(10,2), Base)
+
+    def test_no_args(self):
+        r = Rectangle
+        with self.assertRaises(TypeError):
+            r()
+    
+    def test_one_arg(self):
+        with self.assertRaises(TypeError):
+            Rectangle(1)
+
+    def test_two_args(self):
+        r1 = Rectangle(10, 2)
+        r2 = Rectangle(2, 10)
+        self.assertEqual(r1.id, r2.id -1)
+
+    def test_three_args(self):
+        r1 = Rectangle(2, 2, 4)
+        r2 = Rectangle(4, 4, 2)
+        self.assertEqual(r1.id, r2.id -1)
+
+    def
