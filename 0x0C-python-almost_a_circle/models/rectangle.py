df72c65f8c2dbbe7ed1247d913ea34058d89cf88
victorpropython <victorchibuzor3109@gmail.com>
2024-05-26 13:16:13 +0100

    for task 3, 4, 5

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -35,38 +35,38 @@ class Rectangle(Base):
     
     @property
     def height(self):
-        """Set/get the new REctangle width"""
+        """Set/get the new Rectangle width"""
         return self.__height
     @height.setter
     def height(self, value):
         if type(value) != int:
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value <= 0:
-            raise ValueError("width must be > 0")
+            raise ValueError("height must be > 0")
         self.__height = value 
 
     @property
     def x(self):
-        """Set/get the new REctangle width"""
+        """Set/get the new Rectangle width"""
         return self.__x
     @x.setter
     def x(self, value):
         if type(value) != int:
-            raise TypeError("width must be an integer")
+            raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
     def y(self):
-        """Set/get the new REctangle width"""
+        """Set/get the new Rectangle width"""
         return self.__y
     @y.setter
     def y(self, value):
         if type(value) != int:
-            raise TypeError("width must be an integer")
+            raise TypeError("y  must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("x must be >= 0")
         self.__y = value
     
     def area(self):
