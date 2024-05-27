2feaa41bd0b1dc8c63e9870f271df1a57182999a
victorpropython <victorchibuzor3109@gmail.com>
2024-05-27 03:23:40 +0100

    for square

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -41,7 +41,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         if type(value) != int:
-            raise TypeError("height must be an integer")
+            raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
@@ -69,7 +69,7 @@ class Rectangle(Base):
         if type(value) != int:
             raise TypeError("y  must be an integer")
         if value < 0:
-            raise ValueError("x must be >= 0")
+            raise ValueError("y must be >= 0")
         self.__y = value
 
     def area(self):
